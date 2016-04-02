7d094e500c461d7d10c194b850ac363c5ea468c4
Jesse Caldwell <CollectionOfAtoms@gmail.com>
2016-04-02 09:38:04 -0700

    Add titles to static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "URA | Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "URA | Help"
+  end
+
+  test "sshould get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "title", "URA | About"
   end
 
 end
