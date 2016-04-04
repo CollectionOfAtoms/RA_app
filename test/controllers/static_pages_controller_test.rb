d729bba9b16e6d9218441e07c355347da0b328d4
Jesse Caldwell <CollectionOfAtoms@gmail.com>
2016-04-04 11:18:57 -0700

    Add setup to written tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  #the setup function is called before tests are run
+  # to streamline writing tests that use similar information 
+  def setup  
+    @base_title = "URA"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "URA | Home"
+    assert_select "title", "#{@base_title} | Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "URA | Help"
+    assert_select "title", "#{@base_title} | Help"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "URA | About"
+  	assert_select "title", "#{@base_title} | About"
   end
 
 end
